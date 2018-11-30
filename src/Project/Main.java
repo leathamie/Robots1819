2cf583de35cf61542bbd46156412f49a2e0b8a6c
leathamie <lea.thamie@outlook.fr>
2018-11-30 11:57:24 +0100

    test

--- src/Project/Main.java
+++ src/Project/Main.java
@@ -6,16 +6,13 @@ import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
-import td3.Arreter;
-import td3.Avancer;
-import td3.Eviter;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		EV3ColorSensor c = new EV3ColorSensor(SensorPort.S3);
-		// TODO Auto-generated method stub
+		// test
 		Button.waitForAnyPress();
 		
 
