beaa320322336d3a75de3249b0231d4f7303c24d
leathamie <lea.thamie@outlook.fr>
2018-11-30 11:49:25 +0100

    init main class test

--- src/Project/Main.java
+++ src/Project/Main.java
@@ -3,16 +3,22 @@ package Project;
 import lejos.hardware.Button;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
+import td3.Arreter;
+import td3.Avancer;
+import td3.Eviter;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		EV3ColorSensor c = new EV3ColorSensor(SensorPort.S3);
+		// TODO Auto-generated method stub
+		Button.waitForAnyPress();
 		
-c.close();
+
 	}
 
 }
